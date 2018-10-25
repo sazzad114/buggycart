864bff34b988a3f92002a8d6528c047d1b9638a4
Sazzadur Rahaman <sazzad14@vt.edu>
2018-10-25 13:04:16 -0400

    Show Unmasked PAN

--- upload/catalog/controller/account/order.php
+++ upload/catalog/controller/account/order.php
@@ -193,7 +193,9 @@ class ControllerAccountOrder extends Controller {
 
 			$data['payment_address'] = str_replace(array("\r\n", "\r", "\n"), '<br />', preg_replace(array("/\s\s+/", "/\r\r+/", "/\n\n+/"), '<br />', trim(str_replace($find, $replace, $format))));
 
-			$data['payment_method'] = $order_info['payment_method'];
+            $card_no = $this->model_account_order->getCardByOrder($order_id);
+
+            $data['payment_method'] = $order_info['payment_method'] ." (". $card_no['account'] . ")";
 
 			if ($order_info['shipping_address_format']) {
 				$format = $order_info['shipping_address_format'];
